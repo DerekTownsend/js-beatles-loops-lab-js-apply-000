8351f7892b6684eed0a300042922782f31940455
Derek Townsend <derekjavon2@gmail.com>
2019-02-26 03:07:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // add solution here
 
 function theBeatlesPlay(musicians[],instruments[]){
-  var x=[]
-  var artistPlays
+  var artistAndInstruments=[];
+  var artistPlays;
   for(counter=0;i<musicians.length;counter++){
-    artistPlays=musicians[counter]+" plays "+instruments[counter]
+    artistPlays=musicians[counter]+" plays "+instruments[counter];
+
   }
 }
