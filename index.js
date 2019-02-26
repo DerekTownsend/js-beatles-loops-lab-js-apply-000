64a65c3591d836fe875e6b46da178aa04e900c62
Derek Townsend <derekjavon2@gmail.com>
2019-02-26 03:06:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 function theBeatlesPlay(musicians[],instruments[]){
   var x=[]
   for(counter=0;i<musicians.length;counter++){
-    var artistPlays=musicians[counter]+" plays"+
+    var artistPlays=musicians[counter]+" plays "+instruments[counter]
   }
 }
